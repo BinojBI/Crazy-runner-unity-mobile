3de8ca6d6b129c5a5908b28d5c8d9084a3bbdc9e
BinojBI <binojishara21@gmail.com>
2018-12-11 17:59:16 +0530

    initial commit

---
+++ Assets/moveByTouch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveByTouch : MonoBehaviour {
+
+
+public bool lift = false;
+private Animator anim;
+private void Start() {
+	anim = GetComponent<Animator>();
+}
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+			switch (touch.phase)
+            {
+                //When a touch has first been detected, change the message and record the starting position
+                case TouchPhase.Began:
+                    // Record initial touch position.
+                   
+                    Debug.Log("began");
+					lift = true;
+                    break;
+					 
+
+
+				// case TouchPhase.Moved:
+
+				// Debug.Log("move");
+                //     break;
+
+				case TouchPhase.Ended:
+                    // Report that the touch has ended when it ends
+                    Debug.Log("ending");
+					lift = false;
+                    break;
+
+			}
+		}
+
+		if(lift == true){
+			anim.Play("lift");
+		}
+
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	private Vector3 targetPos;
+	public float yIncremental;
+	
+	public float maxright;
+	public float maxleft;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			//transform.position = Vector2.MoveTowards(targetPos,transform.position, speed* Time.deltaTime );
+
+		if (Input.GetKeyDown(KeyCode.D) && transform.position.x < maxright)
+		{
+			targetPos = new Vector3(transform.position.x + yIncremental ,transform.position.y, transform.position.z );
+			transform.position = targetPos;	
+
+		}else if(Input.GetKeyDown(KeyCode.A) && transform.position.x > maxleft)
+		{
+			targetPos = new Vector3(transform.position.x - yIncremental ,transform.position.y, transform.position.z );
+			transform.position = targetPos;
+		}
+	}
+
+
+}
